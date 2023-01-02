f209ef2b182a02a51d5148b72653adfa9e303cbd
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2023-01-02 09:12:04 -0300

    Update img.service.ts

--- src/app/services/img.service.ts
+++ src/app/services/img.service.ts
@@ -10,27 +10,27 @@ export class ImgService {
   constructor(private http:HttpClient) { }
 
   getImage(){
-    return this.http.get<Imagens[]>("http://18.231.197.66:8080/api/image ");
+    return this.http.get<Imagens[]>("http://18.231.197.66/api/image ");
   }
 
   createImage(payLoad:Imagens){
-    return this.http.post<Imagens>("http://18.231.197.66:8080/api/image", payLoad);
+    return this.http.post<Imagens>("http://18.231.197.66/api/image", payLoad);
   }
 
   getByIdImage(id:number){
-    return this.http.get<Imagens>(`http://18.231.197.66:8080/api/image/${id}`);
+    return this.http.get<Imagens>(`http://18.231.197.66/api/image/${id}`);
   }
 
   updateImage(payLoad:Imagens){
-    return this.http.put(`http://18.231.197.66:8080/api/image/${payLoad.id}`,payLoad);
+    return this.http.put(`http://18.231.197.66/api/image/${payLoad.id}`,payLoad);
   }
 
   deleteImage(id:number){
-    return this.http.delete<Imagens>(`http://18.231.197.66:8080/api/image/${id}`);
+    return this.http.delete<Imagens>(`http://18.231.197.66/api/image/${id}`);
   }
 
   searchImageProduct(premio:number){
-    const baseUrl="http://18.231.197.66:8080/api/imagep/"+premio;
+    const baseUrl="http://18.231.197.66/api/imagep/"+premio;
     return this.http.get<Imagens>(baseUrl);
   }
 }
