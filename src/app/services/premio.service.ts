7af261746699aa3ec7d75d836d796b2403e5f0b4
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2023-01-02 09:14:54 -0300

    Update premio.service.ts

--- src/app/services/premio.service.ts
+++ src/app/services/premio.service.ts
@@ -10,30 +10,30 @@ export class PremioService {
   constructor(private http:HttpClient) { }
 
   getPremio(){
-    return this.http.get<Premio[]>("http://18.231.197.66:8080/api/premio");
+    return this.http.get<Premio[]>("http://18.231.197.66/api/premio");
   }
 
   createPremio(payLoad:Premio){
-    return this.http.post<Premio>("http://18.231.197.66:8080/api/premio", payLoad);
+    return this.http.post<Premio>("http://18.231.197.66/api/premio", payLoad);
   }
 
   getByIdPremio(id:number){
-    return this.http.get<Premio>(`http://18.231.197.66:8080/api/premio/${id}`);
+    return this.http.get<Premio>(`http://18.231.197.66/api/premio/${id}`);
   }
 
   updatePremio(payLoad:Premio){
-    return this.http.put(`http://18.231.197.66:8080/api/premio/${payLoad.id}`,payLoad);
+    return this.http.put(`http://18.231.197.66/api/premio/${payLoad.id}`,payLoad);
   }
 
   deletePremio(id:number){
-    return this.http.delete<Premio>(`http://18.231.197.66:8080/api/premio/${id}`);
+    return this.http.delete<Premio>(`http://18.231.197.66/api/premio/${id}`);
   }
 
   searchPremioValido(valido:number){
-    return this.http.get<Premio>(`http://18.231.197.66:8080/api/premiosearchvalido/${valido}`);
+    return this.http.get<Premio>(`http://18.231.197.66/api/premiosearchvalido/${valido}`);
   }
 
   searchPremio(titulo:string){
-    return this.http.get<Premio>(`http://18.231.197.66:8080/api/premiosearch/${titulo}`);
+    return this.http.get<Premio>(`http://18.231.197.66/api/premiosearch/${titulo}`);
   }
 }
