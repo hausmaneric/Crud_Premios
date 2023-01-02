be1b9ffdf8169a87baf6ddd0d344a4d4465c608e
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2023-01-02 09:15:29 -0300

    Update profissional.service.ts

--- src/app/services/profissional.service.ts
+++ src/app/services/profissional.service.ts
@@ -10,26 +10,26 @@ export class ProfissionalService {
   constructor(private http:HttpClient) { }
 
   getProfissional(){
-    return this.http.get<Profissional[]>("http://18.231.197.66:8080/api/profissional",);
+    return this.http.get<Profissional[]>("http://18.231.197.66/api/profissional",);
   }
 
   createProfissional(payLoad:Profissional){
-    return this.http.post<Profissional>("http://18.231.197.66:8080/api/profissional", payLoad, {observe: 'response'});
+    return this.http.post<Profissional>("http://18.231.197.66f/api/profissional", payLoad, {observe: 'response'});
   }
 
   getByIdProfissional(id:number){
-    return this.http.get<Profissional>(`http://18.231.197.66:8080/api/profissional/${id}`);
+    return this.http.get<Profissional>(`http://18.231.197.66/api/profissional/${id}`);
   }
 
   updateProfissional(payLoad:Profissional){
-    return this.http.put(`http://18.231.197.66:8080/api/profissional/${payLoad.id}`,payLoad);
+    return this.http.put(`http://18.231.197.66/api/profissional/${payLoad.id}`,payLoad);
   }
 
   deleteProfissonal(id:number){
-    return this.http.delete<Profissional>(`http://18.231.197.66:8080/api/profissional/${id}`);
+    return this.http.delete<Profissional>(`http://18.231.197.66/api/profissional/${id}`);
   }
 
   searchProfissional(nome:string){
-    return this.http.get<Profissional>(`http://18.231.197.66:8080/api/profissionalsearch/${nome}`);
+    return this.http.get<Profissional>(`http://18.231.197.66/api/profissionalsearch/${nome}`);
   }
 }
