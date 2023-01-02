9b8fc873144653501f65c052944e7928c1265f83
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2023-01-02 09:15:56 -0300

    Update segmento.service.ts

--- src/app/services/segmento.service.ts
+++ src/app/services/segmento.service.ts
@@ -10,22 +10,22 @@ export class SegmentoService {
   constructor(private http:HttpClient) { }
 
   getSegmento(){
-    return this.http.get<Segmento[]>("http://18.231.197.66:8080/api/segmentos");
+    return this.http.get<Segmento[]>("http://18.231.197.66/api/segmentos");
   }
 
   createSegmento(payLoad:Segmento){
-    return this.http.post<Segmento>("http://18.231.197.66:8080/api/segmentos", payLoad);
+    return this.http.post<Segmento>("http://18.231.197.66/api/segmentos", payLoad);
   }
 
   getByIdSegmento(id:number){
-    return this.http.get<Segmento>(`http://18.231.197.66:8080/api/segmentos/${id}`);
+    return this.http.get<Segmento>(`http://18.231.197.66/api/segmentos/${id}`);
   }
 
   updateSegmento(payLoad:Segmento){
-    return this.http.put(`http://18.231.197.66:8080/api/segmentos/${payLoad.id}`,payLoad);
+    return this.http.put(`http://18.231.197.66/api/segmentos/${payLoad.id}`,payLoad);
   }
 
   deleteSegmento(id:number){
-    return this.http.delete<Segmento>(`http://18.231.197.66:8080/api/segmentos/${id}`);
+    return this.http.delete<Segmento>(`http://18.231.197.66/api/segmentos/${id}`);
   }
 }
